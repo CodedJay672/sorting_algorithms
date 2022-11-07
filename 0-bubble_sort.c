18d41853dc66cb186ce1f48e3b9a08a5a22b33e4
Dojinex <bitrusdauda9118@gmail.com.com>
2022-11-07 07:51:24 -0800

    task 0 commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,52 +1,29 @@
 #include "sort.h"
 
 /**
-<<<<<<< HEAD
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
-=======
- * bubble_sort -> function which sorts an array using bubble sort
- * @array: pointer to the array of integers
- * @size: size of the array
->>>>>>> aa8dea86c400404bc6f47bf8cf4dcc446fe1eaf1
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @array: the array to be sorted
+ * @size: number of elements in @array
  *
  */
 
 void bubble_sort(int *array, size_t size)
 {
-<<<<<<< HEAD
-size_t i, index, tmp = 0;
-if (size < 2)
-return;
-for (i = 0; i < size; i++)
-for (index = 0; index < size; index++)
+int tmp, swapped = 0;
+size_t i, n = size;
+while (swapped == 0)
 {
-if (array[index] > array[index + 1] && array[index + 1])
+swapped = 1;
+for (i = 1; i < n; i++)
+if (array[i - 1] > array[i])
 {
-tmp = array[index];
-array[index] = array[index + 1];
-array[index + 1] = tmp;
+tmp = array[i];
+array[i] = array[i - 1];
+array[i - 1] = tmp;
+swapped = 0;
 print_array(array, size);
 }
 }
-=======
-	size_t i, j;
-
-	for (j = 0; j < size - 1; j++)
-	{
-		for (i = 0; i < size - j - 1; i++)
-		{
-			int first_val = 0;
-
-			if (array[i] > array[i + 1])
-			{
-				first_val = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = first_val;
-				print_array(array, size);
-			}
-		}
-	}
->>>>>>> aa8dea86c400404bc6f47bf8cf4dcc446fe1eaf1
 }
