500324bcddcfca66d147e0ad2cf62ef9139c923d
Dojinex <bitrusdauda9118@gmail.com.com>
2022-11-07 10:36:38 -0800

    task 8 commit

--- sort.h
+++ sort.h
@@ -31,6 +31,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
 
 /* advanced tasks functions */
 void shell_sort(int *array, size_t size);
@@ -42,5 +44,13 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
+
+/* Prototypes for print functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Prototypes for helper functions */
+void swap(int *a, int *b);
+void heapify(int *array, int end, int start, size_t size);
 
 #endif
