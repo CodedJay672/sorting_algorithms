5fda68b00065911bf5f091669197629329e985a5
Dojinex <bitrusdauda9118@gmail.com.com>
2022-11-08 12:08:07 -0800

    task 11 commit

--- sort.h
+++ sort.h
@@ -4,6 +4,23 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ *
+ */
+
+typedef enum bool
+{
+false = 0,
+true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -49,4 +66,4 @@ void sort_deck(deck_node_t **deck);
 void swap(int *a, int *b);
 void heapify(int *array, int end, int start, size_t size);
 
-#endif
+#endif /* SORT_H */
